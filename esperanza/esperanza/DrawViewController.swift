18bf5891a714dc6bbc160c38c5d68216b5c728f2
tokonatsutan <owner_suteburo@yahoo.co.jp>
2016-04-09 23:27:48 +0900

    Added ACEDrawing Sample

---
+++ esperanza/esperanza/DrawViewController.swift
@@ -0,0 +1,42 @@
+//
+//  DrawViewController.swift
+//  esperanza
+//
+//  Created by 影山 大輔 on 2016/04/09.
+//  Copyright © 2016年 tokonatsutan. All rights reserved.
+//
+
+import UIKit
+
+class DrawViewController: UIViewController {
+    
+    @IBOutlet var drawingView: ACEDrawingView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor.whiteColor()
+        
+//        let canvas = UINib(nibName: "CanvasView", bundle: nil).instantiateWithOwner(self, options: nil)[0] as! ACEDrawingView
+//        self.view.addSubview(canvas)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func pen(sender: AnyObject) {
+        self.drawingView.drawTool = ACEDrawingToolTypePen
+    }
+    
+    @IBAction func line(sender: AnyObject) {
+        self.drawingView.drawTool = ACEDrawingToolTypeLine
+    }
+    
+    @IBAction func eraser(sender: AnyObject) {
+        self.drawingView.drawTool = ACEDrawingToolTypeEraser
+    }
+    @IBAction func color(sender: AnyObject) {
+        self.drawingView.lineColor = UIColor.orangeColor()
+    }
+}
